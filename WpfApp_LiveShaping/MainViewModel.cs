f98fb16dbee13c2cd214da3cea7409cef7fb5914
msiddiqi <shujaatned@hotmail.com>
2014-02-27 09:44:41 -0500

    Added Live Sorting support

---
+++ WpfApp_LiveShaping/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_LiveShaping
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            Students = new ObservableCollection<StudentViewModel>();
+            Students.Add(new StudentViewModel { FirstName = "Muhammad", LastName = "Siddiqi" });
+            Students.Add(new StudentViewModel { FirstName = "Kate", LastName = "Hansen" });
+            Students.Add(new StudentViewModel { FirstName = "Vladimir", LastName = "Khan" });
+            
+        }
+
+        public ObservableCollection<StudentViewModel> Students { get; private set; }
+    }
+}
---
+++ WpfApp_LiveShaping/StudentViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_LiveShaping
+{
+    public class StudentViewModel : INotifyPropertyChanged
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (string.Equals(_firstName, value))
+                {
+                    return;
+                }
+
+                _firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (string.Equals(_lastName, value))
+                {
+                    return;
+                }
+
+                _lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
